a32e6908427263b3de84be8846aa9a76f49f02b3
Menghieng Sorn <sornmenghieng@cgg.holdings>
2025-11-12 14:09:32 +0700

    second commit

---
+++ app/Http/Controllers/Usercontroller.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        $users = User::all();
+        return response()->json($users);
+    }
+
+    public function store(Request $request)
+    {
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+
+        return response()->json($user);
+    }
+}
